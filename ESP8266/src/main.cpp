53f8da7c4a2db49e3d5f873e1c9908cd6b098d8f
haochen07 <haoc13@uci.edu>
2017-10-19 16:50:29 -0700

    add MAX30105 sensor module

--- ESP8266/src/main.cpp
+++ ESP8266/src/main.cpp
@@ -2,24 +2,17 @@
 #include "ArduinoJson.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include "MAX30105.h"
+#include <Wire.h>
 
 String deviceName = "CS244";
+MAX30105 particleSensor;
 
 // WiFi settings
 const char *ssid = "UCInet Mobile Access";
 
-void printMacAddress()
+void connectWiFi()
 {
-    byte mac[6];
-    WiFi.macAddress(mac);
-
-    char MAC_char[18]="";
-    for (int i = 0; i < sizeof(mac); ++i)
-        sprintf(MAC_char, "%s%02x:", MAC_char, mac[i]);
-
-    Serial.print("Mac address : ");
-    Serial.print(MAC_char);
-
     WiFi.begin(ssid);
     while (WiFi.status() != WL_CONNECTED)
     {
@@ -36,35 +29,49 @@ void setup()
 {
     // initialize LED digital pin as an output.
     pinMode(LED_BUILTIN, OUTPUT);
-    
     Serial.begin(115200);
     Serial.println("Program started");
-    printMacAddress();
+
+    // connect WiFi
+    connectWiFi();
+
+    // Initialize sensor
+    if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+    {
+        Serial.println("MAX30105 was not found. Please check wiring/power. ");
+        while (1);
+    }
+    
+    //Configure sensor. Use 6.4mA for LED drive
+    particleSensor.setup();
+
+    byte powerLevel = 0x02; //0.4mA - Presence detection of ~4 inch
+    //byte powerLevel = 0x1F; // 6.4mA - Presence detection of ~8 inch
+    //byte powerLevel = 0x7F; //25.4mA - Presence detection of ~8 inch
+    //byte powerLevel = 0xFF; //50.0mA - Presence detection of ~12 inch
+    particleSensor.setPulseAmplitudeRed(powerLevel);
+    particleSensor.setPulseAmplitudeIR(powerLevel);
+    particleSensor.setPulseAmplitudeGreen(powerLevel);
+    particleSensor.setPulseAmplitudeProximity(powerLevel);
 }
 
 void loop()
 {
     // Configure http connection and set headers
     HTTPClient http;
-    http.begin("http://169.234.29.200:8888/");
+    http.begin("http://169.234.17.0:8888/");
     http.addHeader("Content-Type", "application/json");
 
     // Render Json request
     StaticJsonBuffer<300> JSONbuffer;    
     JsonObject& root = JSONbuffer.createObject();
-    root["dataType"] = "Heart Beats";
-    JsonArray& value = root.createNestedArray("value");
-    value.add(66);
-    value.add(67);
-    value.add(68);
-    JsonArray& time = root.createNestedArray("time");
-    time.add("01:00");
-    time.add("02:00");
-    time.add("03:00");
-    
+    root["Red"] = particleSensor.getRed();
+    root["IR"] = particleSensor.getIR();
+
     // Convert Json object to string
     char jsonBuffer[300];
     root.printTo(jsonBuffer, sizeof(jsonBuffer));
+    Serial.println(jsonBuffer);
 
     // Post request, receive http response and print
     int httpCode = http.POST(jsonBuffer);
@@ -81,4 +88,3 @@ void loop()
     digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
 }
-
