b6de52b4fefd7041651686014535e7723c8abfed
zangshayang1 <zangshayang1@hotmail.com>
2017-10-19 22:01:06 -0700

    re-write server side code

---
+++ server/output.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+// module.exports is what you need to make the function available to other modules, when they require("./output.js")
+module.exports.write_to = function(filepath, content) {
+  fs.writeFile(filepath, content, function(err) {
+    if(err) {
+      return console.log(err);
+     }
+     console.log("Create file: " + filepath + " and write: " + content);
+  });
+}
+
+module.exports.append_to = function(filepath, content) {
+  fs.appendFile(filepath, content, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Write: " + content + " to existing file: " + filepath);
+  });
+}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
+// import { write_to } from "output.js"
+const output = require('./output.js');
 const http = require('http');
+const fs = require('fs');
+
+var filepath = "/Users/shayangzang/Desktop/octaCat/CS244Fall2017/project/server/testOut.csv";
+
+header = "RED,IR\n"
+output.write_to(filepath, header);
 
 http.createServer((request, response) => {
   const { headers, method, url } = request;
@@ -10,10 +18,34 @@ http.createServer((request, response) => {
   }).on('end', () => {
     requestBody = Buffer.concat(requestBody).toString();
 
-  console.log("headers: " + headers)
-  console.log("method: " + method)
-  console.log("url: " + url)
-  console.log("requestBody: " + requestBody);
+  // console.log("headers: " + headers)
+  // console.log("method: " + method)
+  // console.log("url: " + url)
+  // console.log("requestBody: " + requestBody);
+
+  JSON.parse(requestBody, (key, value) => {
+    console.log("parsing " + key);
+    if (key == "Red") {
+      console.log("Red value: " + value);
+      // rArr.push(value);
+      var rVal = value;
+    }
+    else if (key == "IR") {
+      console.log("IR value: " + value);
+      // irArr.push(value);
+      var irVal = value;
+    }
+    else if (key == "time") {
+      console.log("time: " + value);
+      var time = value;
+    }
+    // JSON.parse will not stop parsing until it hits the point where key == ""
+    else {
+      console.log("Request Parsing Done.");
+    }
+  })
+  var record = rVal + "," + irVal + "," + time + "\n";
+  output.append_to(filepath, record)
 
 
   response.on('error', (err) => {
@@ -24,9 +56,7 @@ http.createServer((request, response) => {
   response.setHeader('Content-Type', 'application/json');
   // Note: the 2 lines above could be replaced with this next one:
   // response.writeHead(200, {'Content-Type': 'application/json'})
-
   let responseBody = "Greeting from the Cloud.";
-
   const responseWritable = { headers, method, url, responseBody };
 
   // response.write(JSON.stringify(responseBody));
